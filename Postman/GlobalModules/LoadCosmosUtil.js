7a3b7e7170800270c75bb43fc9ab55a438672686
Bjørn Gustafson <bjorn.gustafson@komplettbank.no>
2019-08-01 13:38:43 +0200

    Added global modules for working with Azure Blobs and CosmosDB, as well as some generic util functions

---
+++ Postman/GlobalModules/LoadCosmosUtil.js
@@ -0,0 +1,183 @@
+﻿/* ================================================================================== */
+/*
+/*  Executing this request will update a global variable with a function that returns 
+/*  an object with Cosmos DB utility-functions. The object can be loaded and used in 
+/*  test-scripts, for example like this:
+/*
+/*      let cosmosUtil = eval(pm.globals.get("module:cosmosUtil"));
+/*      let authDetails = cosmosUtil.getAuthDetails(request.url, postman.getEnvironmentVariable("cosmos.masterKey"));
+/*
+/*  IMPORTANT NOTE:
+/*  ---------------------
+/*  The master for this file is kept in "\KB.Postman\GlobalModules\LoadCosmosUtil.js"
+/*  in the "KB.Shared.Testing" git-repo in the "KBShared" project in Azure DevOps.
+/*  Any changes made here must be propagated to the master file!
+/*
+/* ======================================================= */
+
+
+postman.setGlobalVariable("module:cosmosUtil", function loadCosmosUtil() {
+
+    let _logVerbose = false;  // verbose logging OFF by default
+    let _cosmosUtil = {};
+
+    /* ======================================================= */
+    /*
+    /*                      Public methods
+    /*
+    /* ======================================================= */
+
+    /*
+        Creates an auth-token than can be used in the Authorization header to authenticate with the CosmosDb Rest API,
+        as well as timestamp that can be used in the "x-ms-date"-header. The values are returned in an object, defined
+        like this:
+
+        {
+            "authToken" : <token>,
+            "RFC1123time" : <timestamp>
+        }
+
+        Parameters:
+            url         = The url to use to build the token (the protocol and domain will be stripped). Required.
+            masterKey   = The base64-encoded master key used to generate the token. Required.
+            verbose     = Whether to log verbosely. Default is false.
+
+    */
+    _cosmosUtil.getAuthDetails = (url, mastKey, verbose) => {
+
+        if (!url) {
+            console.error("Missing value for required parameter 'url'");
+            return null;
+        }
+
+        if (!mastKey) {
+            console.error("Missing value for required parameter 'mastKey'");
+            return null;
+        }
+        
+        let authDetails = {
+            authToken: null,
+            RFC1123time: null
+        };
+
+        // Whether or not to log verbosely
+        _logVerbose = verbose || _logVerbose;
+
+        // store our date as RFC1123 format for the request
+        var today = new Date();
+        var UTCstring = today.toUTCString();
+        authDetails.RFC1123time = UTCstring;
+
+        //Resolve variables in url:
+        var regex = /\{\{[\w.-]*\}\}/g;
+        vars = url.match(regex);
+        vars.map(v => {
+            var key = v.replace("{{", "").replace("}}", "");
+            var value = postman.getEnvironmentVariable(key);
+            _log(`Replacing ${v} with ${value}`)
+            url = url.replace(v, value);
+        });
+
+        // Strip the url of the hostname up and leading slash
+        var strippedurl = url.replace(new RegExp('^https?://[^/]+/'), '/');
+        _log("Stripped Url = " + strippedurl);
+
+        // push the parts down into an array so we can determine if the call is on a specific item
+        // or if it is on a resource (odd would mean a resource, even would mean an item)
+        var strippedparts = strippedurl.split("/");
+        var truestrippedcount = strippedparts.length - 1;
+
+        // define resourceId/Type now so we can assign based on the amount of levels
+        var resourceId = "";
+        var resType = "";
+
+        // its odd (resource request)
+        if (truestrippedcount % 2) {
+            _log("odd");
+            // assign resource type to the last part we found.
+            resType = strippedparts[truestrippedcount];
+            _log(resType);
+
+            if (truestrippedcount > 1) {
+                // now pull out the resource id by searching for the last slash and substringing to it.
+                var lastPart = strippedurl.lastIndexOf("/");
+                resourceId = strippedurl.substring(1, lastPart);
+                _log(resourceId);
+            }
+        }
+        else // its even (item request on resource)
+        {
+            _log("even");
+            // assign resource type to the part before the last we found (last is resource id)
+            resType = strippedparts[truestrippedcount - 1];
+            _log("resType");
+            // finally remove the leading slash which we used to find the resource if it was
+            // only one level deep.
+            strippedurl = strippedurl.substring(1);
+            _log("strippedurl");
+            // assign our resourceId
+            resourceId = strippedurl;
+            _log("resourceId");
+        }
+
+        // assign our verb
+        var verb = request.method.toLowerCase();
+
+        // assign our RFC 1123 date
+        var date = UTCstring.toLowerCase();
+
+        // parse our master key out as base64 encoding
+        var key = CryptoJS.enc.Base64.parse(mastKey);
+        _log("key = " + key);
+
+        // build up the request text for the signature so can sign it along with the key
+        var text = (verb || "").toLowerCase() + "\n" +
+            (resType || "").toLowerCase() + "\n" +
+            (resourceId || "") + "\n" +
+            (date || "").toLowerCase() + "\n" +
+            "" + "\n";
+        _log("text = " + text);
+
+        // create the signature from build up request text
+        var signature = CryptoJS.HmacSHA256(text, key);
+        _log("sig = " + signature);
+
+        // back to base 64 bits
+        var base64Bits = CryptoJS.enc.Base64.stringify(signature);
+        _log("base64bits = " + base64Bits);
+
+        // format our authentication token and URI encode it.
+        var MasterToken = "master";
+        var TokenVersion = "1.0";
+        var auth = encodeURIComponent("type=" + MasterToken + "&ver=" + TokenVersion + "&sig=" + base64Bits);
+        _log("auth = " + auth);
+
+        authDetails.authToken = auth;
+
+        return authDetails;
+    };
+
+    /* ======================================================= */
+    /*
+    /*                 Private helper methods
+    /*
+    /* ======================================================= */
+
+    function _log(message) {
+        if (_logVerbose) {
+            console.log(message);
+        }
+    }
+
+
+    return _cosmosUtil;
+} + '; loadCosmosUtil();');
+
+/*
+ * Try to load the module using eval, and test the result, to assert that it 
+ * actually can be loaded inside a test-script in Postman.
+ */
+let cosmosUtil = eval(pm.globals.get("module:cosmosUtil"));
+pm.test("Global cosmosUtil loaded successfully", () => {
+    pm.expect(cosmosUtil).to.not.equal(undefined);
+});
---
+++ Postman/GlobalModules/LoadUtil.js
@@ -0,0 +1,141 @@
+﻿/* ================================================================================== */
+/*
+/*  Executing this request will update a global variable with a function that returns 
+/*  an object with general utility-functions. The object can be loaded and used in 
+/*  test-scripts, for example like this:
+/*
+/*      let util = eval(pm.globals.get("module:util"));
+/*      util.isResponse200();
+/*
+/*  IMPORTANT NOTE:
+/*  ---------------------
+/*  The master for this file is kept in "\KB.Postman\GlobalModules\LoadUtil.js"
+/*  in the "KB.Shared.Testing" git-repo in the "KBShared" project in Azure DevOps.
+/*  Any changes made here must be propagated to the master file!
+/*
+/* ======================================================= */
+
+
+postman.setGlobalVariable("module:util", function loadUtil() {
+
+    let _util = {};
+
+    /* ======================================================= */
+    /*
+    /*                      Public methods
+    /*
+    /* ======================================================= */
+
+    /*
+        Returns true for an empty object, or null, or undefined, or nothing specified at all, or any number.
+        Returns true for an actual object, or for a string.
+    */
+    _util.isEmpty = (o) => { return _isEmpty(o); };
+
+    /*
+        Returns true if iteration data has been specified (an input file to a runner). 
+        This can be used to determine if fallback values should be used (when no iteration data exists)
+    */
+    _util.hasIterationData = () => { return !_isEmpty(pm.iterationData.toObject()); };
+
+    /* 
+        Use the following functions to add a test that verifies the responsecode, 
+        and get the test-result as a boolean.
+    */
+    _util.isResponse200 = () => { return _verifyResponseStatusCode(200); };
+    _util.isResponse400 = () => { return _verifyResponseStatusCode(400); };
+    _util.isResponse404 = () => { return _verifyResponseStatusCode(404); };
+    _util.isResponse = (expectedCode) => { return _verifyResponseStatusCode(expectedCode); };
+
+    /*
+        Adds a test that verifies that the content-type header is correct, and that the 
+        response is a valid JSON, with an actual body (not an empty object), and returns 
+        the result of these tests as a boolean.
+        Note:
+        The parameter "expectedContentType" is optional, and can usually be ommitted. It 
+        specifies the expected content-type to look for in the header, and will default
+        to "application/json; charset=utf-8", which is what our API's will return. However,
+        the Cosmos DB API will only return "application/json", and so we need to be able
+        to override the default expected value.
+    */
+    _util.isResponseJson = (expectedContentType) => {
+        expectedContentType = expectedContentType || "application/json; charset=utf-8";
+        if (_verifyResponseContentType(expectedContentType)) {
+            try {
+                let json = pm.response.json(); // Details: http://www.postmanlabs.com/postman-collection/Response.html
+                pm.test("Response has a valid JSON body", () => { pm.expect(_isEmpty(json)).to.be.false; });
+                return true;
+            }
+            catch (e) {
+                pm.test("Response is valid JSON", () => { pm.expect(e).to.be.null; });
+                return false;
+            }
+        }
+        return false;
+    };
+
+    /*
+        Adds a test that verifies that the content-type header is correct, and that the 
+        response size (content-length header or the actual response body) is greater than 0.
+    */
+    _util.isResponsePdf = () => {
+        if (_verifyResponseContentType("application/pdf")) {
+            let size = pm.response.size().body; // The doc says size() returns a number, but the log tells me it's an object. Doc: http://www.postmanlabs.com/postman-collection/Response.html
+            pm.test("Content length > 0", () => { pm.expect(size).to.be.above(0); });
+            return size > 0;
+        }
+        return false;
+    };
+
+    /*
+        Stops the runner (Newman or the Postman Collection Runner), and logs the reason.
+    */
+    _util.stopRunner = (reason) => {
+        console.warn("Stopping runner. Reason: " + reason);
+        postman.setNextRequest(null);
+    };
+
+    /* ======================================================= */
+    /*
+    /*                 Private helper methods
+    /*
+    /* ======================================================= */
+
+    /* 
+        Test for a response status code, but also return the result of that test as a boolean, to enable
+        conditional execution of requests, based on the return value
+    */
+    function _verifyResponseStatusCode(expectedStatusCode) {
+        pm.test("Status code is " + expectedStatusCode, () => { pm.response.to.have.status(expectedStatusCode); });
+        return pm.response.code === expectedStatusCode;
+    }
+
+    function _verifyResponseContentType(expectedContentType) {
+        pm.test("Content-Type is '" + expectedContentType + "'", () => { pm.response.to.have.header("Content-Type", expectedContentType); });
+        return postman.getResponseHeader("Content-Type") === expectedContentType;
+    }
+
+    /* 
+        Returns true if an object is empty. 
+        See https://stackoverflow.com/questions/2340509/javascript-trivia-check-for-equality-against-the-empty-object
+    */
+    function _isEmpty(o) {
+        for (let i in o) {
+            if (o.hasOwnProperty(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return _util;
+} + '; loadUtil();');
+
+/*
+ * Try to load the module using eval, and test the result, to assert that it 
+ * actually can be loaded inside a test-script in Postman.
+ */
+let util = eval(pm.globals.get("module:util"));
+pm.test("Global util loaded successfully", () => {
+    pm.expect(util).to.not.equal(undefined);
+});
